ac92b621ce834971ecc6cf089e402d9a5e0a7943
JuanDavid24 <david.juan90.jd@gmail.com>
2024-06-27 13:58:40 -0300

    rutas y templete inicial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableroComponent, CabeceroComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
