989309b5f95b55b459b670906d88aebff91c9f03
JuanDavid24 <david.juan90.jd@gmail.com>
2024-07-18 15:01:53 -0300

    environments creados

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firestore: {
+        apiKey: "AIzaSyAhPAN2fDBtkr9vUlYaDbOzVr8U-npjSUc",
+        authDomain: "control-clientes-cdf04.firebaseapp.com",
+        projectId: "control-clientes-cdf04",
+        storageBucket: "control-clientes-cdf04.appspot.com",
+        messagingSenderId: "370511644329",
+        appId: "1:370511644329:web:53b920604267737f854bb8"
+      }
+};
