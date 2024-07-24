03b59b87710e0cc948f38450a0bd86118cad7e10
JuanDavid24 <david.juan90.jd@gmail.com>
2024-07-24 16:56:27 -0300

    conexion firebase sin auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableroComponent } from './componentes/tablero/tablero.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableroComponent, CabeceroComponent, PiePaginaComponent],
+  imports: [RouterOutlet, TableroComponent, CabeceroComponent, PiePaginaComponent, NgFor, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'control-clientes';
+  firestore: Firestore = inject(Firestore);
+  clientes$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'clientes')
+    this.clientes$ = collectionData(aCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp(environment.firestore)), provideFirestore(() => getFirestore())]
 };
