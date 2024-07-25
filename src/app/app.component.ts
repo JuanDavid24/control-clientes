670dc2005189e17f3d7d461278dc71f79802b23d
JuanDavid24 <david.juan90.jd@gmail.com>
2024-07-25 14:27:25 -0300

    [FEAT] getClientes v1 con servicio (doc angular/fire + yt)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,24 @@ import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { ClienteServicio } from './servicios/cliente.service';
+import { Cliente } from './modelo/cliente.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, TableroComponent, CabeceroComponent, PiePaginaComponent, NgFor, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [ClienteServicio]
 })
 export class AppComponent {
   title = 'control-clientes';
   firestore: Firestore = inject(Firestore);
-  clientes$: Observable<any[]>;
+  cliente!: Observable<Cliente>;
+  clientes!: Observable<Cliente[]>
 
-  constructor() {
-    const aCollection = collection(this.firestore, 'clientes')
-    this.clientes$ = collectionData(aCollection);
+  constructor(private clienteServicio: ClienteServicio) {
+    this.clientes = this.clienteServicio.getClientes();
   }
 }
