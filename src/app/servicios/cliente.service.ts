1166e8c3199877231df0c155fdd354599d0f0464
JuanDavid24 <david.juan90.jd@gmail.com>
2024-07-26 11:37:13 -0300

    [REFAC] limpieza codigo + injected in root

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -1,39 +1,15 @@
 import { Injectable, inject } from "@angular/core";
-//import { AngularFirestoreCollection, AngularFirestoreDocument } from "@angular/fire/compat/firestore";
 import { Cliente } from "../modelo/cliente.model";
 import { Observable } from "rxjs";
-//import { map } from "rxjs/operators";
 import { Firestore, collection, collectionData, orderBy, query } from "@angular/fire/firestore";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class ClienteServicio{
     private firestore: Firestore = inject(Firestore);
     private clientesCollection = collection(this.firestore, 'clientes');
 
-    //clientesColeccion: AngularFirestoreCollection<Cliente>;
-    //clienteDoc: AngularFirestoreDocument<Cliente>;
-/*
-    constructor(){
-        this.clientesColeccion = db.collection('clientes', ref => ref.orderBy('nombre', 'asc'));
-        
-    }
-*/
     getClientes(): Observable<Cliente[]> {
-        //Obtener los clientes 
-        /*
-        this.clientes = this.clientesColeccion.snapshotChanges().pipe(
-            map( cambios => {
-                return cambios.map(accion => {
-                    const datos = accion.payload.doc.data() as Cliente;
-                    datos.id = accion.payload.doc.id;
-                    return datos
-                })
-            })
-        )
-            */
         const queryFn = query(this.clientesCollection, orderBy('nombre', 'asc'));
         return collectionData(queryFn, {idField: 'id'})
-        //this.clientes = collectionData<Cliente>(this.clientesCollection);
-           
     }
 }
