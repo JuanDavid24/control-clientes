97687401f38223213b6025c8e0a9a40e50d59018
JuanDavid24 <david.juan90.jd@gmail.com>
2024-07-26 11:37:42 -0300

    [FEAT] tabla clientes y btn agregar cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { TableroComponent } from './componentes/tablero/tablero.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { NgFor } from '@angular/common';
-import { CommonModule } from '@angular/common';
-import { ClienteServicio } from './servicios/cliente.service';
-import { Cliente } from './modelo/cliente.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableroComponent, CabeceroComponent, PiePaginaComponent, NgFor, CommonModule],
+  imports: [RouterOutlet, TableroComponent, CabeceroComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  providers: [ClienteServicio]
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'control-clientes';
-  firestore: Firestore = inject(Firestore);
-  cliente!: Observable<Cliente>;
-  clientes!: Observable<Cliente[]>
-
-  constructor(private clienteServicio: ClienteServicio) {
-    this.clientes = this.clienteServicio.getClientes();
-  }
 }
--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteServicio } from '../../servicios/cliente.service';
+import { Cliente } from '../../modelo/cliente.model';
+import { CurrencyPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CurrencyPipe],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit {
+  
+  clientes: Cliente[] = [];
 
+  constructor(private clienteServicio: ClienteServicio) { }
+
+  ngOnInit(): void {
+      this.clienteServicio.getClientes().subscribe(
+        clientes => {
+          this.clientes = clientes
+          this.clientes.forEach (c => console.log(c))
+        }
+      )
+  }
 }
