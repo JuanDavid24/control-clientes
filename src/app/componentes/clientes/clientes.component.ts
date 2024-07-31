eee4756501fc65c8e6b2c314ee9bda5e18eb23f3
JuanDavid24 <david.juan90.jd@gmail.com>
2024-07-31 00:14:29 -0300

    [FEAT] campo nombre validado

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,33 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteServicio } from '../../servicios/cliente.service';
 import { Cliente } from '../../modelo/cliente.model';
-import { CurrencyPipe, NgFor } from '@angular/common';
+import { CurrencyPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [NgFor, CurrencyPipe, RouterLink],
+  imports: [NgFor, CurrencyPipe, RouterLink, FormsModule, NgClass, NgIf],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent implements OnInit {
-  
-  clientes: Cliente[] = [];
 
-  constructor(private clienteServicio: ClienteServicio) { }
 
-  ngOnInit(): void {
-      this.clienteServicio.getClientes().subscribe(
-        clientes => {
+cliente: Cliente = {
+  nombre: '',
+  apellido: '',
+  email: '',
+  saldo: 0
+}
+clientes: Cliente[] = [];
+
+constructor(private clienteServicio: ClienteServicio) { }
+
+ngOnInit(): void {
+  this.clienteServicio.getClientes().subscribe(
+    clientes => {
           this.clientes = clientes
           this.clientes.forEach (c => console.log(c))
         }
       )
+    }
+    
+    getSaldoTotal() {
+      return this.clientes.reduce(
+        (acumSaldo, cliente) => acumSaldo + (cliente.saldo || 0), 0)
+      }
+      
+    agregar(formCliente: NgForm) {
+      console.log("agregar cliente");
+    }
   }
-
-   getSaldoTotal() {
-    return this.clientes.reduce(
-      (acumSaldo, cliente) => acumSaldo + (cliente.saldo || 0), 0)
-  }
-}
+    
