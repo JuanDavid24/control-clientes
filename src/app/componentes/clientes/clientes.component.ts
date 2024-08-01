2fa7eda4bff0c6784dfdf06f2daf5210e425f26e
JuanDavid24 <david.juan90.jd@gmail.com>
2024-08-01 15:30:50 -0300

    [REF] cambios menores en identado

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -14,51 +14,51 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ClientesComponent implements OnInit {
 
-cliente: Cliente = {
-  nombre: '',
-  apellido: '',
-  email: '',
-  saldo: 0
-}
-clientes: Cliente[] = [];
-alertVisible: boolean = false;
-@ViewChild("clienteForm") clienteForm !: NgForm;
-@ViewChild("botonCerrarModal") botonCerrarModal !: ElementRef;
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+  }
+  clientes: Cliente[] = [];
+  alertVisible: boolean = false;
+  @ViewChild("clienteForm") clienteForm !: NgForm;
+  @ViewChild("botonCerrarModal") botonCerrarModal !: ElementRef;
 
-constructor(private clienteServicio: ClienteServicio) { }
+  constructor(private clienteServicio: ClienteServicio) { }
 
-ngOnInit(): void {
-  this.clienteServicio.getClientes().subscribe(
-    clientes => {
-          this.clientes = clientes
-          this.clientes.forEach (c => console.log(c))
-        }
-      )
-    }
+  ngOnInit(): void {
+    this.clienteServicio.getClientes().subscribe(
+      clientes => {
+            this.clientes = clientes
+            this.clientes.forEach (c => console.log(c))
+      }
+    )
+  }
     
-    getSaldoTotal() {
-      return this.clientes.reduce(
-        (acumSaldo, cliente) => acumSaldo + (cliente.saldo || 0), 0)
-      }
+  getSaldoTotal() {
+    return this.clientes.reduce(
+      (acumSaldo, cliente) => acumSaldo + (cliente.saldo || 0), 0)
+  }
       
-    agregar(formCliente: NgForm) {
-      if (formCliente.invalid) {
-        //Error en formulario 
-        this.alertVisible = true;
-        setTimeout(() => {
-          this.alertVisible = false;
-        }, 4000); 
-      } 
-      else {
-        //Agregar cliente
-        this.clienteServicio.addCliente(this.cliente);
-        this.clienteForm.resetForm();
-        this.cerrarModal()
-      }
-    }
-
-    private cerrarModal():void {
-      this.botonCerrarModal.nativeElement.click()
+  agregar(formCliente: NgForm) {
+    if (formCliente.invalid) {
+      //Error en formulario 
+      this.alertVisible = true;
+      setTimeout(() => {
+        this.alertVisible = false;
+      }, 4000); 
+    } 
+    else {
+      //Agregar cliente
+      this.clienteServicio.addCliente(this.cliente);
+      this.clienteForm.resetForm();
+      this.cerrarModal()
     }
   }
+
+  private cerrarModal():void {
+    this.botonCerrarModal.nativeElement.click()
+  }
+}
     
