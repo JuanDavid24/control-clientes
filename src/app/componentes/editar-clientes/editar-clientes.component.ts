c8bd3ab59aab48776b6fcb6b42ca180c5aaf5e1f
JuanDavid24 <david.juan90.jd@gmail.com>
2024-08-01 15:31:08 -0300

    [FEAT] obtener cliente por id

--- src/app/componentes/editar-clientes/editar-clientes.component.ts
+++ src/app/componentes/editar-clientes/editar-clientes.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Cliente } from '../../modelo/cliente.model';
+import { NgForm } from '@angular/forms';
+import { ClienteServicio } from '../../servicios/cliente.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-clientes',
@@ -7,6 +11,30 @@ import { Component } from '@angular/core';
   templateUrl: './editar-clientes.component.html',
   styleUrl: './editar-clientes.component.css'
 })
-export class EditarClientesComponent {
+export class EditarClientesComponent implements OnInit {
 
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+  }
+
+  id!: string;
+  constructor(private clienteServicio: ClienteServicio,
+              private router: Router,
+              private route: ActivatedRoute
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.id = this.route.snapshot.params['id'] //obtengo id cliente de la ruta
+
+    await this.obtenerCliente();
+    console.log(this.cliente);
+    
+  }
+
+  async obtenerCliente(): Promise<void> {
+    this.cliente = await this.clienteServicio.getCliente(this.id)
+  }
 }
--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from "@angular/core";
 import { Cliente } from "../modelo/cliente.model";
 import { Observable } from "rxjs";
-import { DocumentReference, Firestore, addDoc, collection, collectionData, orderBy, query } from "@angular/fire/firestore";
+import { DocumentReference, Firestore, addDoc, collection, collectionData, getDoc, orderBy, query } from "@angular/fire/firestore";
+import { doc } from "firebase/firestore";
 
 @Injectable({providedIn: 'root'})
 export class ClienteServicio{
@@ -13,7 +14,17 @@ export class ClienteServicio{
         return collectionData(queryFn, {idField: 'id'})
     }
 
+    async getCliente(id: string): Promise<Cliente> {
+        const docRef = this.getDocRef(id);
+        const docData = await getDoc(docRef);
+        return docData.data() as Cliente
+    }
+
     addCliente(cliente: Cliente): Promise<DocumentReference> {
         return addDoc(this.clientesCollection, cliente)
     }
-}
+
+    private getDocRef(id: string) {
+        return doc(this.firestore, 'clientes', id)
+    }
+} 
